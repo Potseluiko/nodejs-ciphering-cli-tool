6b531d4036dd8cc398b4ae51765851b4418588fc
Alexandra Ilyashenko <sasha.ilyashenko@onde.app>
2021-11-21 17:23:02 +0300

    feat: add tests for helper.js

--- src/ciphers/helper.test.js
+++ src/ciphers/helper.test.js
@@ -1,9 +1,27 @@
 import { encodeHelper } from "./helper.js"
 
 describe("Rot8 encode", () => {
-  test("should not change value", () => {
+  test("should not change value (NOT latin letters)", () => {
+    const genCharFn = (charCode, startCharCode, endCharCode) => String.fromCharCode(charCode + 100)
+
+    expect(encodeHelper("Здесь был я!", genCharFn)).toBe("Здесь был я!")
+  })
+
+  test("should not change value (latin letters)", () => {
     const genCharFn = (charCode, startCharCode, endCharCode) => String.fromCharCode(charCode)
 
     expect(encodeHelper("ABCD", genCharFn)).toBe("ABCD")
+  })
+
+  test("should change letters to '*' if it is in the interval", () => {
+    const genCharFn = (charCode, startCharCode, endCharCode) => {
+      if (charCode >= startCharCode && charCode <= endCharCode) {
+        return "*"
+      }
+
+      return String.fromCharCode(charCode)
+    }
+
+    expect(encodeHelper("Здесь ABCD находится!", genCharFn)).toBe("Здесь **** находится!")
   })
 })
